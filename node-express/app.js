82e42d4cb32bb698c83a9157bcb0d069e351c9b8
dgnzz <dioguin333@gmail.com>
2026-04-24 11:50:18 -0300

    subindo arquivos node e java

---
+++ node-express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/users', userRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
